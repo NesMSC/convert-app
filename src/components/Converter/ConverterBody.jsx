4eb8a5fabbd87b5aaa59ffc9af932bd911d2dbb4
Nestor Saavedra <nestormsc.@gmail.com>
2023-04-30 10:52:59 -0400

    Converter body and coin fields

---
+++ src/components/Converter/ConverterBody.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ConverterBody = ({ children }) => {
+  return (
+    <div className='w-full p-2 border-b flex flex-col items-center'>
+        { children }
+    </div>
+  )
+}
+
+export default ConverterBody
--- src/components/Converter/index.jsx
+++ src/components/Converter/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ConverterHeader from './ConverterHeader'
+import ConverterBody from './ConverterBody'
+import CoinField from '../CoinField'
+
+import { ArrowLongDownIcon } from '@heroicons/react/24/solid';
 
 const Converter = () => {
+  const coin = {
+    name: "BTC"
+  }
+
   return (
-    <div className='w-11/12 h-96 flex flex-col bg-white border rounded-2xl shadow-md mt-12'>
+    <div className='w-11/12 flex flex-col bg-white border rounded-2xl shadow-md pb-4 mt-12'>
         <ConverterHeader 
             title={"Criptocurrencies Converter"}
             subtitle={"Your Ultimate Currency Conversion Tool"}
         />
+        <ConverterBody>
+          <CoinField coin={coin} value={"0.0"}/>
+          <div className='w-8 h-8 p-2 mt-2 rounded-full bg-purple-300 hover:opacity-80 cursor-pointer flex items-center justify-center'>
+            <ArrowLongDownIcon />
+          </div>
+          <CoinField coin={coin} value={"0.0"}/>
+        </ConverterBody>
     </div>
   )
 }
